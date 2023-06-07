7efa71543a351a614b015025568d123ab2c53051
colloso999 <tripperskripper@gmail.com>
2023-06-07 23:44:38 +0300

    modified def main function

--- weather.py
+++ weather.py
@@ -1,6 +1,44 @@
+#!/usr/bin/python3
 import requests
-from dotenv 
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: float 
+
 def get_lan_lon(city_name, state_code, country_code, API_key):
-    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?
-            q={city_name},{state_code},{country_code}&limit={limit}&
-            appid={API_key}')
+    resp = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}'
+    response = requests.get(resp).json()
+    data = response[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    return lat, lon
+
+def get_current_weather(lat, lon, API_key):
+    resp = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric'
+    response = requests.get(resp).json()
+    data = WeatherData(
+        main=response.get('weather')[0].get('main'),
+        description=response.get('weather')[0].get('description'),
+        icon=response.get('weather')[0].get('icon'),
+        temperature=response.get('main').get('temp')
+    )
+    return data
+    # print(response)
+
+def main(city_name, state_name, country_name):
+    lat, lon = get_lan_lon(city_name, state_name, country_name, api_key)
+    weather_data = get_current_weather(lat, lon, api_key)
+    return weather_data
+
+if __name__ == '__main__':
+    lat, lon = get_lan_lon('Toronto', 'ON', 'Canada', api_key)
+    print(get_current_weather(lat, lon, api_key))
+# print(get_lan_lon('Toronto', 'ON', 'Canada', api_key))
